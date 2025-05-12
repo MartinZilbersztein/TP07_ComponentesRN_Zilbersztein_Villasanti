d9006ac494972de2070652cfcb22bb45761b8cb9
MartinZilber <48520155@est.ort.edu.ar>
2025-05-12 10:36:41 -0300

    UI casi finalizado 2.0

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Image, TextInput, TouchableOpacity, SafeAreaView, ImageBackground, Alert, Pressable, Text, View } from 'react-native';
+
 const imgExterna= {uri: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'};
 
 export default function App() {
@@ -8,22 +9,24 @@ export default function App() {
   const alertar = ()=>{
     Alert.alert(text);//NO USAR ALERT
   }
+  const cambiarFondo = ()=>{
+  }
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.view}>
+    <ImageBackground style={styles.container} resizeMode='cover' source={{uri: 'https://media.istockphoto.com/id/1362473128/es/vector/viejo-r%C3%BAstico-sucio-desordenado-desgastado-escala-de-grises-claro-o-blanco-color-grunge.jpg?s=612x612&w=0&k=20&c=hEF1k6eMGmIETny4tE9irRSqQrnG07m_CL7k2MFCF3s='}}>
+      <SafeAreaView style={styles.view} id='mainContainer'>
         <Image source={imgExterna} style={styles.Image}/>
-        <Text style={{fontSize:25}}>Martín Zilbersztein</Text>
+        <Text style={{fontSize:25, fontFamily:'times new roman'}}>Martín Zilbersztein</Text>
         <Text style={{fontSize:20}}>Frontend developer</Text>
-        <TextInput style={styles.textInput} placeholder='Enter a message' onChangeText={useText} value={text}/>
+        <TextInput style={[styles.textInput, styles.entrada]} placeholder='Enter a message' onChangeText={useText} value={text}/>
         <TouchableOpacity style={styles.touchableOpacity} onPress={alertar}>
-          <Text style={styles.botonTexto}>Contactar</Text>
+          <Text style={[styles.botonTexto, {color: 'white'}]}>Contactar</Text>
         </TouchableOpacity>
-        <Pressable style={styles.pressable}>
-          <Text>Hola!</Text>
+        <Pressable style={[styles.pressable, styles.entrada]} onPress={cambiarFondo}>
+          <Text style={styles.botonTexto}>Ver portfolio</Text>
         </Pressable>
         <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
@@ -36,7 +39,7 @@ const styles = StyleSheet.create({
     display:'flex'
   },
   view:{
-    backgroundColor:'white',
+    backgroundColor: 'white',
     width:'70%',
     height:'55%',
     alignItems:'center',
@@ -48,15 +51,18 @@ const styles = StyleSheet.create({
     borderRadius:100,
     marginTop:'5%'
   },
-  textInput:{
+  entrada:{
     borderBottomWidth : 1.0,
     borderTopWidth:1.0,
     borderLeftWidth:1.0,
     borderRightWidth:1.0,
-    borderRadius:9,
+    borderRadius:10
+  },
+  textInput:{
     width:'80%',
     height:'9%',
-    marginTop:'5%'
+    marginTop:'5%',
+    padding:'10'
   },
   touchableOpacity:{
     marginTop: 10,
@@ -67,20 +73,14 @@ const styles = StyleSheet.create({
     borderRadius:10
   },
   botonTexto:{
-    color: 'white',
     textAlign:'center',
     fontSize:20
   },
   pressable:{
     marginTop:10,
     backgroundColor:'white',
-    borderRadius:10,
-    borderBottomWidth : 1.0,
-    borderTopWidth:1.0,
-    borderLeftWidth:1.0,
-    borderRightWidth:1.0,
-    width:'80%',
-    height:'10%',
+    width: '80%',
+    height: '10%',
     justifyContent:'center',
     alignItems:'center'
   }
