726334c0a3ed1cfac25dbcaccd84e5060960e17c
MartinZilber <48520155@est.ort.edu.ar>
2025-05-23 12:09:44 -0300

    Proyecto 'finalizado' (la statusbar no funciona como debería

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StyleSheet, SafeAreaView, ImageBackground } from 'react-native';
+import { StyleSheet, SafeAreaView, ImageBackground, View } from 'react-native';
 import Tarjeta from './Tarjeta.js';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <ImageBackground style={styles.container} resizeMode='cover' source={{uri: 'https://media.istockphoto.com/id/1362473128/es/vector/viejo-r%C3%BAstico-sucio-desordenado-desgastado-escala-de-grises-claro-o-blanco-color-grunge.jpg?s=612x612&w=0&k=20&c=hEF1k6eMGmIETny4tE9irRSqQrnG07m_CL7k2MFCF3s='}}>
-      <SafeAreaView style={styles.view} id='mainContainer'>
-       <Tarjeta />
-       <StatusBar style='auto'/>
-      </SafeAreaView>
-    </ImageBackground>
+    <View style={styles.aa}>
+      <ImageBackground style={styles.imagen} resizeMode='cover' source={{uri: 'https://media.istockphoto.com/id/1362473128/es/vector/viejo-r%C3%BAstico-sucio-desordenado-desgastado-escala-de-grises-claro-o-blanco-color-grunge.jpg?s=612x612&w=0&k=20&c=hEF1k6eMGmIETny4tE9irRSqQrnG07m_CL7k2MFCF3s='}}>
+        <SafeAreaView style={styles.view} id='mainContainer'>
+        <Tarjeta />
+        </SafeAreaView>
+      </ImageBackground>
+      <StatusBar backgroundColor='green'/>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  aa:{
+    flex:1,
+  },
+  imagen: {
     flex: 1,
     backgroundColor: 'lightgray',
     alignItems: 'center',
